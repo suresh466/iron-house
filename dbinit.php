a5a40c35448fb25802840cb9b0dd04daa427d0bb
Suresh <suresh.thagunna77@gmail.com>
2024-02-05 18:12:36 -0500

    improve dbinit script

--- dbinit.php
+++ dbinit.php
@@ -6,17 +6,20 @@ define('DB_PASSWORD', '');
 define('DB_HOST', 'localhost');
 
 // try connecting to the server
-$conn = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD)
-    or die('Could not connect to the server: ' . mysqli_connect_error());
+try {
+$conn = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD);
+} catch (mysqli_sql_exception $e) {
+    die("Could not connect to the server: " . mysqli_connect_error());}
 
 // create the database
-if (mysqli_query($conn, "CREATE DATABASE IF NOT EXISTS iron_house")) {
-    echo "Database created successfully";
-} else {
-    die("Error creating database, maybe it exists already " . mysqli_error($conn));
-}
+try {
+mysqli_query($conn, "CREATE DATABASE iron_house");
+// use the created iron_house database
+mysqli_query($conn, "USE iron_house");
+echo "<p>Database created successfully</p>";
+} catch (mysqli_sql_exception $e) {
+    die("Error creating database: " . mysqli_error($conn));}
 
-// create the table.
 $create_ironsuits_table = "CREATE TABLE ironsuits (
     ironsuit_id INT(6) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     ironsuit_name VARCHAR(30) NOT NULL,
@@ -26,7 +29,12 @@ $create_ironsuits_table = "CREATE TABLE ironsuits (
     product_added_by VARCHAR(50) DEFAULT 'Suresh Thagunna'
     )";
 
-mysqli_query($conn, $create_ironsuits_table);
+// create the table.
+try {
+    mysqli_query($conn, $create_ironsuits_table);
+    echo "<p>Table created successfully</p>";
+} catch (mysqli_sql_exception $e) {
+    die("Error creating table: " . mysqli_error($conn));}
 
 // add initial data
 $insert_ironsuits = "INSERT INTO ironsuits (ironsuit_name, ironsuit_description, ironsuit_quantity_available, ironsuit_price) VALUES
@@ -36,3 +44,10 @@ $insert_ironsuits = "INSERT INTO ironsuits (ironsuit_name, ironsuit_description,
     ('Mark 4', 'Now comes the fourth generation with compact form, truly powerful weapon system which can take over alien ships, an truly industrial design', 22, 75.00),
     ('Mark 5', 'The holy grail of iron suits, the ultimate Mark 5, this is where nano technology comes in, making the suit a part of you', 15, 85.00)
     ";
+
+try {
+    mysqli_query($conn, $insert_ironsuits);
+    echo "<p>Initial data inserted successfully</p>";
+} catch (mysqli_sql_exception $e) {
+    die("Error inserting data: " . mysqli_error($conn));
+}
